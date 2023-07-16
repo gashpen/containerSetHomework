664d06e02078a5fde162a9df5b58bf07ca8dc644
gashpen <constantinent@mail.ru>
2023-07-16 15:57:14 +0300

    done

---
+++ homework/src/js/Team.class.js
@@ -0,0 +1,29 @@
+/* eslint-disable guard-for-in */
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+class Team {
+  constructor() {
+    this.members = new Set();
+  }
+
+  add(character) {
+    if (this.members.has(character)) {
+      throw new Error('Персонаж уже существует!');
+    }
+    return this.members.add(character);
+  }
+
+  addAll(...character) {
+    for (let i = 0; i < character.length; i += 1) {
+      this.members.add(i);
+    }
+  }
+
+  toArray(character) {
+    return Array.from(this.members.add(character));
+  }
+}
+
+const myTeam = new Team();
+
+export default myTeam;
---
+++ homework/src/js/__tests__/Team.class.test.js
@@ -0,0 +1,6 @@
+/* eslint-disable no-unused-vars */
+import myTeam from '../Team.class';
+
+test('test', () => {
+  expect(myTeam.add());
+});
